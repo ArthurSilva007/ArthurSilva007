e36b8048ce559aeb21b87192178642ff8470e728
🧑‍💻 Anderson Silva <166549172+ArthurSilva007@users.noreply.github.com>
2026-03-19 22:20:18 -0300

    Delete repet_txt.py

--- repet_txt.py
+++
@@ -1,7 +0,0 @@
-texto = input("Digite uma string: ")
-numero = int(input("Digite um número inteiro: "))
-
-# Repetir a string o número de vezes especificado
-resultado = texto * numero
-
-print("A string repetida é :", resultado)
